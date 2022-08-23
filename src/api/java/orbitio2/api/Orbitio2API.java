89620fbcd1bbd2893ef07f1afe9488dd633d58ad
Cinciullain <casula.alessandro@outlook.com>
2022-08-23 15:53:44 +0200

    Started the base for a future API implementation

---
+++ src/api/java/orbitio2/api/Orbitio2API.java
@@ -0,0 +1,16 @@
+package orbitio2.api;
+
+public class Orbitio2API
+{
+
+    private Orbitio2API()
+    {
+    }
+
+    /**
+     *  La versione della API non è necessariamente uguale a quella della mod
+     */
+    public static final String API_VERSION = "0.0.1";
+    public static final String ORBITIO2_MODID = "orbitio2";
+
+}
